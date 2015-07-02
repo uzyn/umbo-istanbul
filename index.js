16f56c07d0421a195eae2e9cbd7d59b856bdc731
U-Zyn Chua <chua@uzyn.com>
2015-07-03 01:10:56 +0800

    Parses JSON coverage

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 #!/usr/bin/env node
 var program = require('commander');
+var path = require('path');
 
 program
   .version('0.0.1')
   .option('-j, --json-summary <path>', 'Path to json-summary reported by istanbul. Defaults to coverage/coverage-summary.json', 'coverage/coverage-summary.json')
+  .option('-t, --type <type>', 'Coverage type: lines, statements, functions or branches. Defaults to lines.', /^(lines|statements|functions|branches)$/i, 'lines')
+  .option('-k, --key <private key>', 'Umbo private key.')
   .parse(process.argv);
 
-console.log(program.jsonSummary);
+var summary = require(path.resolve(program.jsonSummary));
+
+console.log(program.type);
+console.log(summary.total[program.type.toLowerCase()].pct);
